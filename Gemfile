8bfb2a7df4013a75f9e49bf2eb8e3c4f66f4f7b5
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2019-07-21 21:34:21 +0100

    Use rack-test for testing
    The test is still stub

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "rack-test", "~> 1.1", :group => :test
--- test/integration_test.rb
+++ test/integration_test.rb
@@ -1,14 +1,26 @@
 require "minitest/autorun"
+require "bundler/setup"
+require "rack/test"
 require "json"
 
 class IntegrationTest < Minitest::Test
   def test_list_users
-    response = get("/users.json")
+    api.get("/users.json")
     expected = { data: [] }
-    assert_equal expected.to_json, response
+    assert_equal expected.to_json, api.last_response.body
   end
 
-  def get(path)
-    { data: [] }.to_json
+  def api
+    @api ||= Rack::Test::Session.new(Rack::MockSession.new(app))
+  end
+
+  def app
+    Rack::Builder.new.run(lambda { |env|
+      [
+        200,
+        { "Content-Type" => "application/json" },
+        %W{{"data": []}}
+      ]
+    })
   end
 end
